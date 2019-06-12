8bbb1719c7440bc02e05be0af728a06322975c19
econstantine <e2constantine@gmail.com>
2019-06-11 21:08:57 -0400

    11June2019 - GI Haul changes including javascript.

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+===============================================================================================
+Component:  script.js
+Purpose:    Javascript meant for the GI Haul website
+Programmer: Ed Constantine
+
+Modification History:
+Date             Programmer           Description
+--------         -------------        --------------------
+June 11, 2019    EConstantine         Initial Creation
+===============================================================================================
+*/
+$(document).ready(function() {
+
+   $('checkAvail').click(function(){
+     $('#checkAvail').toggle();
+   });
+   console.log("done");
+});
